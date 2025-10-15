5502cda4146036dc19fb40082e21ebeca3dfd055
RaakeshB <raakesh.balaji@gmail.com>
2025-10-15 14:10:00 +0530

    Add app module and routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MovieRowComponent } from './components/movie-row/movie-row.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+import { SearchComponent } from './pages/search/search.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    MovieRowComponent,
+    MovieCardComponent,
+    HomeComponent,
+    MovieDetailComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
